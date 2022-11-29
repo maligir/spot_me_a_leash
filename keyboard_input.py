5e77572d64bfbf16170cc70cc3be07e5e37c894b
anushkashah <anushkashah654@gmail.com>
2022-11-29 12:33:45 -0600

    reverted

--- keyboard_input.py
+++ keyboard_input.py
@@ -6,13 +6,13 @@ if __name__ == '__main__':
     ros_pub = rospy.Publisher('itl_keyboard', String, queue_size=10)
     rospy.init_node('itl_keyboard_pub')
     rate = rospy.Rate(60)
-    filedescriptors = termios.tcgetattr(sys.stdin)
-    tty.setcbreak(sys.stdin)
-    x = 'w'
+    # filedescriptors = termios.tcgetattr(sys.stdin)
+    # tty.setcbreak(sys.stdin)
+    # x = 'w'
     while not rospy.is_shutdown():
         # x = sys.stdin.read(1)[0]
-        ros_pub.publish(x)
-        # ros_pub.publish(input())
+        # ros_pub.publish(x)
+        ros_pub.publish(input())
         rate.sleep()
 
 
