e72cb6783751d3635cb785a0d7527386fc46d0de
anushkashah <anushkashah654@gmail.com>
2022-11-29 10:31:31 -0600

    made a class

--- itl.py
+++ itl.py
@@ -3,36 +3,35 @@ from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import PointCloud2
 
-global direction
 
-def callback(data):
-    direction = data.data
-    print(direction)
-    rospy.loginfo("I heard %s", direction)
-    
-def print_dir():
-    if (direction != None):
-        print(direction)
+class itl_run:
 
-if __name__ == '__main__':
-    ros_pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
-    rospy.init_node('itl_keyboard_sub', anonymous=True)
-    ros_key = rospy.Subscriber('itl_keyboard', String, callback)
-    rate = rospy.Rate(60)
-    while not rospy.is_shutdown():
-        # print(direction)
-        # print_dir()
-        msg = Twist()
-        msg.angular.x = 0.0
-        msg.angular.y = 0.0
-        msg.angular.z = 0.0
-        msg.linear.x = -0.3
-        msg.linear.y = 0.0
-        msg.linear.z = 0.0
-        ros_pub.publish(msg)
-        # rospy.spin()
-        rate.sleep()
+    def __init__(self) -> None:
+        self.direction = ""
+
+    def callback(self, data):
+        self.direction = data.data
+        print(self.direction)
+        rospy.loginfo("I heard %s", self.direction)
+
+    def run_prog(self):
+        ros_pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+        rospy.init_node('itl_keyboard_sub', anonymous=True)
+        ros_key = rospy.Subscriber('itl_keyboard', String, self.callback)
+        rate = rospy.Rate(60)
+        while not rospy.is_shutdown():
+            print(self.direction)
+            msg = Twist()
+            msg.angular.x = 0.0
+            msg.angular.y = 0.0
+            msg.angular.z = 0.0
+            msg.linear.x = 0.3
+            msg.linear.y = 0.0
+            msg.linear.z = 0.0
+            ros_pub.publish(msg)
+            # rospy.spin()
+            rate.sleep()
 
 
-    # rospy.init_node('spot_cmd_vel')
+        # rospy.init_node('spot_cmd_vel')
 
