d5ee6442dbc385ab19e77473c17435a37e25e1b6
anushkashah <anushkashah654@gmail.com>
2022-11-29 10:04:49 -0600

    more testing with sub/pub in one file

--- itl.py
+++ itl.py
@@ -14,17 +14,19 @@ if __name__ == '__main__':
     ros_pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
     rospy.init_node('itl_keyboard_sub', anonymous=True)
     ros_key = rospy.Subscriber('itl_keyboard', String, callback)
-    # rate = rospy.Rate(60)
-    print(direction)
-    msg = Twist()
-    msg.angular.x = 0.0
-    msg.angular.y = 0.0
-    msg.angular.z = 0.0
-    msg.linear.x = 0.0
-    msg.linear.y = 0.0
-    msg.linear.z = 0.0
-    ros_pub.publish(msg)
-    rospy.spin()
+    rate = rospy.Rate(60)
+    while not rospy.is_shutdown():
+        print(direction)
+        msg = Twist()
+        msg.angular.x = 0.0
+        msg.angular.y = 0.0
+        msg.angular.z = 0.0
+        msg.linear.x = 0.0
+        msg.linear.y = 0.0
+        msg.linear.z = 0.0
+        ros_pub.publish(msg)
+        rospy.spin()
+        rate.sleep()
 
 
     # rospy.init_node('spot_cmd_vel')
