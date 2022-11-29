3dd541276f65bec2d5ce11096abdca0477f4804f
anushkashah <anushkashah654@gmail.com>
2022-11-28 18:34:31 -0600

    adding a timer and z

--- itl.py
+++ itl.py
@@ -13,16 +13,16 @@ def user_input(msg: Twist, can_get: int) -> Twist:
             pass
         elif (u_input == 'w'):
             msg.linear.x = 0.3
-            msg.linear.y = 0.0
+            msg.linear.z = 0.0
         elif (u_input == 's'):
             msg.linear.x = -0.3
-            msg.linear.y = 0.0   
+            msg.linear.z = 0.0   
         elif (u_input == 'a'):
             msg.linear.x = 0.0
-            msg.linear.y = -0.1
+            msg.linear.z = -0.2
         elif (u_input == 'd'):
             msg.linear.x = 0.0
-            msg.linear.y = 0.1
+            msg.linear.z = 0.2
  
     return msg
 
