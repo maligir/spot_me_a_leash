2c9ca05d700f295993f535308414e4e99f5beca5
anushkashah <anushkashah654@gmail.com>
2022-11-30 16:05:14 -0600

    changing itl in order to create a more natural way to send inputs

---
+++ automatic_itl.py
@@ -0,0 +1,54 @@
+import rospy
+from std_msgs.msg import String
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import PointCloud2
+
+
+class itl_run:
+
+    def __init__(self) -> None:
+        self.direction = ""
+        self.msg = Twist()
+        self.msg.angular.x = 0.0
+        self.msg.angular.y = 0.0
+        self.msg.angular.z = 0.0
+        self.msg.linear.x = 0.0
+        self.msg.linear.y = 0.0
+        self.msg.linear.z = 0.0
+
+    def callback(self, data):
+        self.direction = data.data
+        print(self.direction)
+        rospy.loginfo("I heard %s", self.direction)
+
+    def run_prog(self):
+        ros_pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+        rospy.init_node('itl_keyboard_sub', anonymous=True)
+        ros_key = rospy.Subscriber('itl_keyboard', String, self.callback)
+        rate = rospy.Rate(60)
+        while not rospy.is_shutdown():
+            print(self.direction)
+            if (self.direction == ''):
+                pass
+            elif (self.direction == 'w'):
+                self.msg.linear.x += 0.1
+                self.msg.angular.z += 0.0
+            elif (self.direction == 's'):
+                self.msg.linear.x += -0.1
+                self.msg.angular.z += 0.0   
+            elif (self.direction == 'a'):
+                self.msg.linear.x += 0.0
+                self.msg.angular.z += 0.1
+            elif (self.direction == 'd'):
+                self.msg.linear.x += 0.0
+                self.msg.angular.z += -0.1
+            elif (self.direction == 'z'):
+                self.msg.linear.x = 0.0
+                self.msg.angular.z = 0.0
+            self.direction = ''
+            ros_pub.publish(self.msg)
+            rate.sleep()
+if __name__ == '__main__':
+    app_prog = itl_run()
+    app_prog.run_prog()
+        # rospy.init_node('spot_cmd_vel')
