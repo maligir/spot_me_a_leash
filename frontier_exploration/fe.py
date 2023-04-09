95efa66ed11748638520acaee6dd3f1acd95ca3e
Rahul Maligi <maligir@utexas.edu>
2023-04-09 10:58:15 -0500

    locations

--- frontier_exploration/fe.py
+++ frontier_exploration/fe.py
@@ -61,7 +61,7 @@ class fe_run:
         max_index = np.argmax(self.open_list["dist"])
         self.move_info["dist"] = self.open_list["dist"][max_index]/data.info.width
         self.move_info["rad"] = self.open_list["rad"][max_index]
-        rospy.loginfo("Moving from %s %s to %s %s %s", cur_x, cur_y, self.move_info["dist"], self.move_info["rad"], count)
+        rospy.loginfo("Moving from %s %s to %s %s %s %s", cur_x, cur_y, self.move_info["dist"], self.move_info["rad"], max_index/data.info.height, max_index%data.info.width)
         
         # mutate close list with the dist and rad of the frontier
         self.closed_list["dist"] = self.closed_list["dist"] - self.move_info["dist"]
