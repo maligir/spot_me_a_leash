d6cc66eb4edd5390879b3be9d44b8eda102065d3
Rahul Maligi <maligir@utexas.edu>
2023-04-09 00:00:07 -0500

    made most changes to fe.py

--- frontier_exploration/fe.py
+++ frontier_exploration/fe.py
@@ -1,36 +1,68 @@
 # this file is frontier exploration (this runs every time new map is generated)
 import rospy
+import numpy as np
 from nav_msgs.msg import OccupancyGrid
+from geometry_msgs.msg import Twist
 
 class fe_run:
     def __init__(self) -> None:
-        pass
+        self.open_list = {"dist": [], "rad": []}
+        self.move_info = {"dist": 0, "rad": 0}
+        self.msg = Twist()
+        self.msg.angular.x = 0.0
+        self.msg.angular.y = 0.0
+        self.msg.angular.z = 0.0
+        self.msg.linear.x = 0.0
+        self.msg.linear.y = 0.0
+        self.msg.linear.z = 0.0
+        # self.closed_list = {"dist": [], "rad": []}
     
     def callback(self, data):
-        # file1 = open("map_data.txt","w")
-        # file1.write(str(data))
-        # file1.write(str(data.data))
         # convert data to 2d matrix
-        temp = []
-        for i in range(0, data.info.height):
-            temp.append(data.data[i*data.info.width:(i+1)*data.info.width])
+        # data.data is a 1d array in row major order
+        # first row is data.data[0:data.info.width]
+        # -1 means unknown
+        # 0 means free
+        # 100 means occupied
+        
+        # get the robots position in the occupancy grid
         cur_x = int(data.info.origin.position.x / data.info.resolution)
         cur_y = int(data.info.origin.position.y / data.info.resolution)
+        # find all the frontiers relative to the robot
         for i in range(0, data.info.height):
-            print(temp[i])
-            if 0 in temp[i]:
-                rospy.loginfo("I heard %s %s", "0found", i)
-            if 100 in temp[i]:
-                rospy.loginfo("I heard %s %s", "100 found", i)
-            break
-        rospy.loginfo("I heard %s %s", 100, cur_y)
+            for j in range(0, data.info.width):
+                if data.data[i*data.info.width + j] == 0:
+                    # find euclidean distance to robot
+                    dist = ((cur_x - i)**2 + (cur_y - j)**2)**0.5
+                    if dist == 0:
+                        continue
+                    if cur_x-i/cur_y-j < 0:
+                        dist = -dist
+                    if cur_y-j == 0:
+                        rad = 0
+                    else:
+                        rad = np.arctan((cur_x - i) / (cur_y - j))
+                    self.open_list["dist"].append(dist)
+                    self.open_list["rad"].append(rad)
+        # select the farthest frontier
+        max_index = self.open_list["dist"].index(max(self.open_list["dist"]))
+        self.move_info["dist"] = self.open_list["dist"][max_index]
+        self.move_info["rad"] = self.open_list["rad"][max_index]
+        # rospy.loginfo("I heard %s %s", 100, cur_y)
         pass
     
     def run_prog(self):
+        ros_pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
         map_sub = rospy.Subscriber('/map', OccupancyGrid, self.callback)
-        rospy.init_node('itl_keyboard_sub', anonymous=True)
+        rospy.init_node('map_sub', anonymous=True)
         rate = rospy.Rate(60)
         while not rospy.is_shutdown():
+            # figure out moving calculations (should be trial and error)
+            # there are two ways to approach this
+            # 1. start with dist speed and keep decreasing speed until 0 (looks like its exploring kinda lmao)
+            # 2. start with x speed and y rad and keep for s amount of time (more accurate in terms of distance)
+            self.msg.linear.x = self.move_info["dist"]
+            self.msg.angular.z = self.move_info["rad"]
             rate.sleep()
         pass
     
@@ -47,7 +79,7 @@ if __name__ == "__main__":
 # repeat below steps with current map as the input
 
 # find the frontiers
-# with ocupancy grid check for borders with -1
+# with ocupancy grid check for borders with 0
 # add all frontiers to frontier list (priority queue)
 # closest frontier is at front of the list
 
